32669c06fe5c074d5a410b293987a30b582779b8
Zulfurix <zulfurix@gmail.com>
2020-01-03 21:18:47 +0000

    Option to prevent automatic seat shuffling

--- RoleplayMenu/RoleplayMenu/MenuHandler.cs
+++ RoleplayMenu/RoleplayMenu/MenuHandler.cs
@@ -14,12 +14,14 @@ namespace RoleplayMenuClient
         Menu menu;
         private MenuController menuController;
         public static Menu destinationsMenu;
+        Menu miscSettingsMenu;
 
         public MenuHandler()
         {
             // Menus
             menu = new Menu("Roleplay Menu");
             destinationsMenu = new Menu("Destinations");
+            miscSettingsMenu = new Menu("Misc Settings");
 
             // Create menu controller
             if (menuController == null)
@@ -27,10 +29,12 @@ namespace RoleplayMenuClient
                 menuController = new MenuController();
                 MenuController.AddMenu(menu);
                 MenuController.AddMenu(destinationsMenu);
+                MenuController.AddMenu(miscSettingsMenu);
                 MenuController.MenuAlignment = MenuController.MenuAlignmentOption.Right;
                 MenuController.MenuToggleKey = Control.MultiplayerInfo;
                 MenuController.EnableMenuToggleKeyOnController = false;
                 MenuController.MainMenu = menu;
+                MenuController.AddSubmenu(menu, miscSettingsMenu);
             }
 
             // Main menu items
@@ -41,14 +45,51 @@ namespace RoleplayMenuClient
             };
             menu.AddMenuItem(taxiItem);
 
+            MenuItem miscSettingsItem = new MenuItem("Miscellaneous Settings", "Configure various miscellaneous settings.")
+            {
+                Enabled = true,
+            };
+            menu.AddMenuItem(miscSettingsItem);
+
+            // Misc Settings items
+            MenuCheckboxItem seatShuffleItem = new MenuCheckboxItem("Disable passenger seat shuffling",
+                "Disables the automatic seat shuffling the passenger performs if the driver seat is empty", true)
+            {
+                Enabled = true,
+            };
+            miscSettingsMenu.AddMenuItem(seatShuffleItem);
+
             // Main menu events
             menu.OnItemSelect += (_menu, _item, _index) =>
             {
                 if (_item == taxiItem)
                 {
                     UIHandler.ShowQuickNotification("A taxi will be dispatched to your location shortly.");
+                    menu.CloseMenu();
                     TriggerEvent("RoleplayMenu:SendTaxiToPlayer");
+                } 
+                else if (_item == miscSettingsItem)
+                {
+                    menu.CloseMenu();
+                    miscSettingsMenu.OpenMenu();
                 }
+            };
+
+            // Misc settings menu events
+
+            miscSettingsMenu.OnCheckboxChange += (_sender, _item, _index, _checked) =>
+            {
+                Debug.WriteLine("Shitty");
+                if (_item == seatShuffleItem)
+                {
+                    Debug.WriteLine("Selected item");
+                    VehicleSettings.DisableSeatShuffle = seatShuffleItem.Checked;
+                }
+            };
+
+            miscSettingsMenu.OnMenuClose += (_sender) =>
+            {
+                miscSettingsMenu.ParentMenu.OpenMenu();
             };
 
             // Destination menu events
---
+++ RoleplayMenu/RoleplayMenu/VehicleSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace RoleplayMenuClient
+{
+    class VehicleSettings : BaseScript
+    {
+        private static bool disableSeatShuffle = true;
+
+        public VehicleSettings()
+        {
+            RegisterScript(this);
+            Tick += VehicleSettingsTick;
+        }
+
+        public static bool DisableSeatShuffle
+        {
+            get { return disableSeatShuffle; }
+            set { disableSeatShuffle = value; }
+        }
+
+        public async Task VehicleSettingsTick()
+        {
+            await Delay(0);
+
+            Debug.WriteLine("Stopping Shuffle 0");
+            if (disableSeatShuffle && Game.PlayerPed.IsInVehicle())
+            {
+                Debug.WriteLine("Stopping Shuffle 1");
+                if (Game.PlayerPed.SeatIndex == VehicleSeat.RightFront)
+                {
+                    Debug.WriteLine("Stopping Shuffle 2");
+                    if (GetIsTaskActive(Game.PlayerPed.Handle, 165))
+                    {
+                        Debug.WriteLine("Stopping Shuffle 3");
+                        Game.PlayerPed.SetIntoVehicle(Game.PlayerPed.CurrentVehicle, Game.PlayerPed.SeatIndex);
+                    }
+                }
+            }
+        }
+    }
+}
